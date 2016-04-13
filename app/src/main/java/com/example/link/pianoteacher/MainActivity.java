6ffb0f7b945fac31401f82853f5931786cb0aa57
sly-loki <you@example.com>
2016-04-13 23:07:03 +0300

    add class with keyboard related constants

--- app/src/main/java/com/example/link/pianoteacher/MainActivity.java
+++ app/src/main/java/com/example/link/pianoteacher/MainActivity.java
@@ -20,7 +20,24 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.LogRecord;
+
+class MusicBox {
+    public static final int WHITE_NOTE_COUNT = 52;
+    public static final int NOTE_COUNT = 88;
+    public static final int BLACK_NOTE_COUNT = NOTE_COUNT - WHITE_NOTE_COUNT;
+    public static final int NOTES_IN_OCTAVE = 12;
+    public static final int WHITE_NOTES_IN_OCTAVE = 7;
+    public static final int C1_NOTE_INDEX = 40;
+    public static final int C2_NOTE_INDEX = C1_NOTE_INDEX + NOTES_IN_OCTAVE;
+    public static final int C3_NOTE_INDEX = C2_NOTE_INDEX + NOTES_IN_OCTAVE;
+    public static final int C4_NOTE_INDEX = C3_NOTE_INDEX + NOTES_IN_OCTAVE;
+    public static final int C5_NOTE_INDEX = C4_NOTE_INDEX + NOTES_IN_OCTAVE;
+    public static final ArrayList<Integer> RELATIVE_BLACK_NOTE_INDEXES = new ArrayList<>(Arrays.asList(1,3,6,8,10));
+    public static final int[] OCTAVE_INDEX_TO_WHITE_INDEX = {0,0,1,1,2,3,3,4,4,5,5,6,6,7};
+}
 
 class StoveViewer extends View {
     Paint p = new Paint();
