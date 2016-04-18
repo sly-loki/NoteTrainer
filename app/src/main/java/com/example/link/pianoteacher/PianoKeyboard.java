ae1e4573c172bfbcfcbadaf26fb1ecb0b47c7003
sly-loki <you@example.com>
2016-04-18 22:11:10 +0300

    keyboard: now all keyboard draws all keys

--- app/src/main/java/com/example/link/pianoteacher/PianoKeyboard.java
+++ app/src/main/java/com/example/link/pianoteacher/PianoKeyboard.java
@@ -17,6 +17,102 @@ class PianoKeyboard extends View {
 
     public static final int NOTE_PRESSED = 1;
     public static final int NOTE_RELEASED = 0;
+    public static final int FULL_OCTAVE_COUNT = 7;
+
+    abstract class Octave {
+        protected int startNote;
+        Octave(int startNote) {
+            this.startNote = startNote;
+        }
+
+        abstract public void draw(Canvas canvas, float x);
+
+        protected void drawBlackNote(Canvas canvas, int state, float left, float bottom, float width, float height) {
+            Paint paint;
+            if (state == NOTE_RELEASED)
+                paint = p;
+            else
+                paint = activePaint;
+            canvas.drawRect(left, bottom, left + width, bottom + height, paint);
+            canvas.drawRect(left, bottom, left + width, bottom + height, strokePaint);
+        }
+
+        protected void drawWhiteNote(Canvas canvas, int state, float left, float bottom, float width, float height) {
+            Paint paint;
+            if (state == NOTE_RELEASED)
+                paint = strokePaint;
+            else
+                paint = activePaint;
+            canvas.drawRect(left, bottom, left+width, bottom+height, paint);
+            canvas.drawRect(left, bottom, left + width, bottom + height, strokePaint);
+        }
+    }
+
+    class FullOctave extends Octave {
+        FullOctave(int startNote) {
+            super(startNote);
+        }
+
+        @Override
+        public void draw(Canvas canvas, float x) {
+            float note_width = getWidth() / MusicBox.WHITE_NOTE_COUNT;
+            float black_note_width = note_width * 0.6f;
+            float black_note_height = getHeight() * 0.7f;
+
+            float currentNoteLeft = x;
+            for (int i = 0; i < MusicBox.NOTES_IN_OCTAVE; i++) {
+                if (!MusicBox.RELATIVE_BLACK_NOTE_INDEXES.contains(i)) {
+                    drawWhiteNote(canvas, notes[startNote + i], currentNoteLeft, 0, note_width, getHeight());
+                    currentNoteLeft += note_width;
+                }
+            }
+
+            currentNoteLeft = x;
+            for (int i = 0; i < MusicBox.NOTES_IN_OCTAVE; i++) {
+                if (MusicBox.RELATIVE_BLACK_NOTE_INDEXES.contains(i)) {
+                    drawBlackNote(canvas, notes[startNote + i], currentNoteLeft + note_width * 0.6f, 0,
+                            black_note_width, black_note_height);
+                    currentNoteLeft += note_width;
+                } else if (i == 4) {
+                    currentNoteLeft += note_width;
+                }
+            }
+        }
+    }
+
+    class SubContrOctave extends Octave {
+        SubContrOctave() {
+            super(0);
+        }
+
+        @Override
+        public void draw(Canvas canvas, float x) {
+            float note_width = getWidth() / MusicBox.WHITE_NOTE_COUNT;
+            float black_note_width = note_width * 0.6f;
+            float black_note_height = getHeight() * 0.7f;
+
+            drawWhiteNote(canvas, notes[0], x, 0, note_width, getHeight());
+            drawWhiteNote(canvas, notes[2], x+note_width, 0, note_width, getHeight());
+            drawBlackNote(canvas, notes[1], x + note_width * 0.6f, 0,
+                    black_note_width, black_note_height);
+        }
+    }
+
+    class FifthOctave extends Octave {
+        FifthOctave(int startNote) {
+            super(startNote);
+        }
+
+        @Override
+        public void draw(Canvas canvas, float x) {
+            float note_width = getWidth() / MusicBox.WHITE_NOTE_COUNT;
+            drawWhiteNote(canvas, notes[MusicBox.NOTE_COUNT - 1], x, 0, note_width, getHeight());
+        }
+    }
+
+    SubContrOctave subContrOctave = new SubContrOctave();
+    FullOctave[] fullOctaves = new FullOctave[8];
+    FifthOctave fifthOctave = new FifthOctave(87);
 
     public PianoKeyboard(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -25,70 +121,23 @@ class PianoKeyboard extends View {
         strokePaint.setStyle(Paint.Style.STROKE);
         activePaint.setColor(Color.MAGENTA);
 
+        for (int i = 0; i < FULL_OCTAVE_COUNT; i++) {
+            fullOctaves[i] = new FullOctave(3 + i*MusicBox.NOTES_IN_OCTAVE);
+        }
+
         for(int i = 0; i < MusicBox.NOTE_COUNT; i++) {
             notes[i] = NOTE_RELEASED;
         }
     }
 
-    private void drawBlackNote(Canvas canvas, int state, float left, float bottom, float width, float height) {
-        Paint paint;
-        if (state == NOTE_RELEASED)
-            paint = p;
-        else
-            paint = activePaint;
-        canvas.drawRect(left, bottom, left + width, bottom + height, paint);
-        canvas.drawRect(left, bottom, left + width, bottom + height, strokePaint);
-    }
-
-    private void drawWhiteNote(Canvas canvas, int state, float left, float bottom, float width, float height) {
-        Paint paint;
-        if (state == NOTE_RELEASED)
-            paint = strokePaint;
-        else
-            paint = activePaint;
-        canvas.drawRect(left, bottom, left+width, bottom+height, paint);
-        canvas.drawRect(left, bottom, left + width, bottom + height, strokePaint);
-    }
-
-    private int calcNoteState(int octave, int note, boolean isBlack)
-    {
-        int start = 3 + 12 * octave;
-        int[] helperWhite = {0,2,4,5,7,9,11};
-        int[] helperBlack = {1,3,0,6,8,10,0};
-
-        if (isBlack)
-            start += helperBlack[note];
-        else
-            start += helperWhite[note];
-        if (start >= MusicBox.NOTE_COUNT)
-            return NOTE_RELEASED;
-        return notes[start];
-    }
-
-    private void drawOctave(Canvas canvas, int num, float left, float bottom, float width, float height) {
-        float note_width = width / 7.0f;
-        float black_note_width = note_width * 0.6f;
-        float black_note_height = height * 0.7f;
-
-        for (int i = 0; i < 7; i++) {
-            float currentNoteLeft = left + note_width * i;
-            drawWhiteNote(canvas, calcNoteState(num, i, false), currentNoteLeft, bottom, note_width, height);
-        }
-
-        for (int i = 0; i < 7; i++) {
-            float currentNoteLeft = left + note_width * i;
-            if (i != 2 && i != 6)
-                drawBlackNote(canvas, calcNoteState(num, i, true), currentNoteLeft + note_width * 0.6f, bottom,
-                        black_note_width, black_note_height);
-        }
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
-        float octaveWidth = (getWidth()-20) / 8;
-        for (int i = 0; i < 8; i++) {
-            drawOctave(canvas, i, 10 + octaveWidth * i, 10, octaveWidth, getHeight() - 10);
+        float noteWidth = (getWidth()) / MusicBox.WHITE_NOTE_COUNT;
+        subContrOctave.draw(canvas, 0);
+        for (int i = 0; i < FULL_OCTAVE_COUNT; i++) {
+            fullOctaves[i].draw(canvas, noteWidth*2 + i * MusicBox.WHITE_NOTES_IN_OCTAVE * noteWidth);
         }
+        fifthOctave.draw(canvas, noteWidth*2 + FULL_OCTAVE_COUNT * MusicBox.WHITE_NOTES_IN_OCTAVE * noteWidth);
     }
 
     public void setNoteState(int note, int state) {
